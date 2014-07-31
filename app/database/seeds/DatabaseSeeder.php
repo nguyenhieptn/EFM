3685a957b68ddcdc4fe4e16c3736e7be674b13e0
nguyenhieptn <nguyenhieptn@yahoo.com>
2014-07-31 10:12:19 +0700

    update db

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,9 +10,10 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-
-		//$this->call('UserTableSeeder');
+        /*
+		$this->call('UserTableSeeder');
 		$this->call('CategoriesTableSeeder');
+        */
 	}
 
 }
--- app/database/seeds/categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'table'=>'categories',
-    array('name'=>'Ca Nhan'),
-    array('name'=>'Vui Choi/Giai Tri'),
-    array('name'=>'Mua Sam'),
-    array('name'=>'Hieu Hy/Tham hoi'),
-);
