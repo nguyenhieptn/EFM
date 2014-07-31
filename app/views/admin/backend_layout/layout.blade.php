dd8a74f9474e6fba16d2389024132db87644b1e7
nguyenhieptn <nguyenhieptn@yahoo.com>
2014-07-31 23:03:07 +0700

    test view url

--- app/views/admin/backend_layout/layout.blade.php
+++ app/views/admin/backend_layout/layout.blade.php
@@ -32,7 +32,7 @@
 </head>
 <body class="skin-blue">
 <!-- header -->
-{{ View::make('admin\backend_layout\header') }}
+{{ View::make('header') }}
 
 <div class="wrapper row-offcanvas row-offcanvas-left">
     <aside class="left-side sidebar-offcanvas">
