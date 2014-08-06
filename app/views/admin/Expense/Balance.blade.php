f53de7f35ed6b81a9ca491aa571762408005340e
dieulinhtbn <dieulinhtbn@gmail.com>
2014-08-06 11:34:53 +0700

    s

--- app/views/admin/Expense/Balance.blade.php
+++ app/views/admin/Expense/Balance.blade.php
@@ -4,7 +4,7 @@
             <ul>
                 <li id="money_in" class="money"><span class="number">{{ number_format($totalIncomes,0,'','.') }} </span><span>Money In</span></li>
                 <li id="money_out"><span class="number">{{ number_format($totalExpenses,0,'','.') }} </span><span>Money Out</span></li>
-                <li>---------------</li>
+                <li><hr></li>
                 <li id="savings" class="moneytotal"><span class="number">{{ number_format($totalIncomes-$totalExpenses,0,'','.') }}</span> <span>Savings</span></li>
 
             </ul>
