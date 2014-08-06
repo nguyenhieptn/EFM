43b3d775e20afc9cc8b0da8ac82d1c4888469182
dieulinhtbn <dieulinhtbn@gmail.com>
2014-08-06 11:06:13 +0700

    style

--- app/views/admin/Expense/Balance.blade.php
+++ app/views/admin/Expense/Balance.blade.php
@@ -2,10 +2,10 @@
     <div class="box-body">
         <div id="jmbalance">
             <ul>
-                <li>{{ number_format($totalIncomes,0,'','.') }}</li>
-                <li>{{ number_format($totalExpenses,0,'','.') }}</li>
+                <li id="money_in" class="money">{{ number_format($totalIncomes,0,'','.') }} <span>Money In</span></li>
+                <li id="money_out">{{ number_format($totalExpenses,0,'','.') }} <span>Money Out</span></li>
                 <li>---------------</li>
-                <li>{{ number_format($totalIncomes-$totalExpenses,0,'','.') }}</li>
+                <li id="savings" class="moneytotal">{{ number_format($totalIncomes-$totalExpenses,0,'','.') }} <span>Savings</span></li>
 
             </ul>
         </div>
